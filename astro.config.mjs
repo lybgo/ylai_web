736d6df0d7143fd6a23d8295e528e22044b49c1d
lyb_dragon16u_pc <hilyb@qq.com>
2026-01-11 23:39:11 +0800

    支持GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 替换为你的 GitHub 域名
+  site: 'https://lybgo.github.io',
+  // 替换为你的仓库名称
+  base: '/ylai_web',
   vite: {
     plugins: [tailwindcss()]
   }
